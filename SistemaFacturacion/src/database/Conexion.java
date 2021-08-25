bc55d9278330c3d9b1c0cb717e758fe94c3825d0
Brayan_Alvarez <10brayanalvarez@gmail.com>
2021-08-25 16:31:49 -0600

    Revert "Merge branch 'release' into Brayan"
    This reverts commit 1a25adfc255b081922dd7a8646f649de5e53cba6.

--- SistemaFacturacion/src/database/Conexion.java
+++ SistemaFacturacion/src/database/Conexion.java
@@ -5,7 +5,6 @@
  */
 package database;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,26 +16,43 @@ import javax.swing.JOptionPane;
  */
 
 public class Conexion {
-    public static void main(String[] args) {
-        System.out.println("Hola");
+    
+    private final String DRIVER = "com.mysql.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/";
+    private final String DB = "dbsistema";
+    private final String USER = "root";
+    private final String PASSWORD = "";
+    
+    public Connection cadena;
+    public static Conexion instancia;
+    
+    private Conexion() {
+        this.cadena = null;
+    }
+    
+    public Connection conectar() {
+        try {
+            Class.forName(DRIVER);
+            this.cadena = DriverManager.getConnection(URL + DB, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "CONEXION::conectar-> "+e.getMessage());
+            System.exit(0); //FINALIZAR EL PROGRAMA
+        }
+        return this.cadena;
+    }
+    
+    public void desconectar() {
+        try {
+            this.cadena.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "CONEXION::desconectar-> "+e.getMessage());
+        }
+    }
+    
+    public synchronized static Conexion getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
     }
 }
- public void desconectar(){
-     try {
-         this.cadena.close();
-     } catch (SQLException e) {
-         JOptionPane.showMessageDialog(null, "CONEXION:: desconectar-> "+e.getMessage());
-         
-     }
- }
- public synchronized static Conexion getInstancia(){
-     if (instancia == null) {
-         instancia = new Conexion();
-     }
-     return instancia;
- }
-        
-        
-        
-}
-
